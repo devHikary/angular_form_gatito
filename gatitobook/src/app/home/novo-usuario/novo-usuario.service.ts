8397826a5db82cdc3af17164fc1be51a840e58c8
devHikary <wrkkarina@gmail.com>
2021-11-13 19:49:40 -0300

    Serviço de Novo Usuário

---
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,15 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastrarNovoUsuario(novousuario: NovoUsuario){
+    return this.httpClient.post('http://localhost:3000/user/signup', novousuario)
+  }
+}
