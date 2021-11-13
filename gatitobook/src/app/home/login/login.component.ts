a3596e072dc55852bd39c02b806e670dfa911be6
devHikary <wrkkarina@gmail.com>
2021-11-12 21:04:27 -0300

    api

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario: string = '';
+  senha:string = ''
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    console.log({senha: this.senha, login: this.usuario})
+  }
 }
