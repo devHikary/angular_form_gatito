29f21b46292174f056580c0998b63fe492fec96a
devHikary <wrkkarina@gmail.com>
2021-11-14 11:21:51 -0300

    Validação de Formulário

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,7 @@ import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -26,6 +27,9 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator],[this.usuarioExistenteServive.usuarioJaExiste()]],
       password: [''],
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
     });
   }
 
